ec5f6472eaebe9a21c9ee422fa071c8d6a785dfb
salvador alemany <126738234+ImperiumRandom@users.noreply.github.com>
2023-09-03 01:26:48 -0400

    Solved anagram problem (was able to sort each string and see if they equaled each other)

--- LeetCodeProblems/Valid Anagram (Arrays and Hashing)/Source.cpp
+++ LeetCodeProblems/Valid Anagram (Arrays and Hashing)/Source.cpp
@@ -1,5 +1,6 @@
 #include <iostream>
 #include <string>
+#include <algorithm>
 
 
 // decided not to use big 0(n²) - sort letters then check with big O(n)
@@ -10,8 +11,25 @@ class Solution {
 public:
     bool isAnagram(std::string s, std::string t) {
 
+        // checks if size is same, this means it can be the same string
+        if (s.size() != t.size()) {
+
+            return false;
+
+        }
+
+        std::sort(t.begin(), t.end());
+
+        std::sort(s.begin(), s.end());
+
+        return s == t;
+
+
     }
+
+
 };
+
 
 
 
@@ -22,23 +40,24 @@ public:
 
 int main() {
 
+    std::string originalForm;
+    std::string anagram;
 
+    std::cout << "Please enter a string\n\n";
 
+    std::getline(std::cin, originalForm);
 
+    system("cls");
 
+    std::cout << "Please enter a anagram\n\n";
 
+    std::getline(std::cin, anagram);
 
+    system("cls");
+    
+    Solution test;
 
-
-
-
-
-
-
-
-
-
-
+    std::cout << test.isAnagram(originalForm, anagram);
 
 
 
